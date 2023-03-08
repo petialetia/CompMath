0e60033325838308df4ceb727cb3f9de055f208c
petialetia <petialetia@yandex.ru>
2023-03-09 02:47:21 +0300

    better

--- 1/CholeskyDecomposition.py
+++ 1/CholeskyDecomposition.py
@@ -4,13 +4,9 @@ import math
 def main():
     print(solveSLAECholesky(numpy.array([[1, 0], [0, 1]]), [5, 6]))
 
-    #assert((solveSLAECholesky(numpy.array([[1, 0], [0, 1]]), [5, 6]) == [5, 6]).all())
-
     return 0
 
 def solveSLAELowerTriangular(coefficients_matrix, constant_terms):
-    #print(coefficients_matrix)
-    
     result = numpy.zeros(len(constant_terms))
     
     for i in range(len(constant_terms)):
@@ -19,7 +15,6 @@ def solveSLAELowerTriangular(coefficients_matrix, constant_terms):
         for j in range(i):
             sum += result[j] * coefficients_matrix[i][j]
 
-        #print(coefficients_matrix[i][i])
         result[i] = (constant_terms[i] - sum) / coefficients_matrix[i][i]
 
     return result
@@ -38,7 +33,6 @@ def solveSLAEUpperLowerTriangular(coefficients_matrix, constant_terms):
     return result
 
 def solveSLAECholesky(coefficients_matrix, constant_terms):
-    #print(numpy.linalg.cholesky(coefficients_matrix))
     factorization = factorizeMatrixCholesky(coefficients_matrix)
 
     itermidiate_constant_terms = solveSLAELowerTriangular(factorization, constant_terms)
@@ -62,23 +56,6 @@ def factorizeMatrixCholesky(matrix):
                     factorization[j][j]
         factorization[i][i] = math.sqrt(matrix[i][i] - \
                 sum(factorization[i][j] ** 2 for j in range(i)))
-
-    """for i in range(matrix_dimensionality):
-        sum = 0
-
-        for j in range(i):
-            sum += factorization[i][j] ** 2
-
-        factorization[i][i] = math.sqrt(matrix[i][i] - sum)
-
-    for i in range(matrix_dimensionality):
-        for j in range(i):
-            sum = 0
-
-            for k in range(j):
-                sum += factorization[i][k] * factorization[j][k]
-
-            factorization[i][j] = (matrix[i][j] - sum) / factorization[j][j]"""
 
     return factorization
 
