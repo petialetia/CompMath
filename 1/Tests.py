4ecf71e34d3a95d193640579a97a0926a644d98b
petialetia <petialetia@yandex.ru>
2023-03-09 03:07:04 +0300

    better

--- 1/Tests.py
+++ 1/Tests.py
@@ -3,7 +3,7 @@ import unittest
 from CholeskyDecomposition import *
 
 class CholeskyDecompositionTestCase(unittest.TestCase):
-   def testIdentityMatrix(self):
+    def testIdentityMatrix(self):
        coefficients_matrix = numpy.array([[1, 0], [0, 1]])
        constant_terms = [5, 6]
 
@@ -12,6 +12,16 @@ class CholeskyDecompositionTestCase(unittest.TestCase):
        
        result = solveSLAECholesky(coefficients_matrix, constant_terms)
        self.assertTrue((result == [5, 6]).all())
+
+    def testSimpltMatrix(self):
+       coefficients_matrix = numpy.array([[2, 1], [1, 2]])
+       constant_terms = [3, 4]
+
+       self.assertTrue((factorizeMatrixCholesky(coefficients_matrix) == \
+                numpy.linalg.cholesky(coefficients_matrix)).all())
+       
+       result = solveSLAECholesky(coefficients_matrix, constant_terms)
+       self.assertTrue(numpy.allclose(result, [0.66666667, 1.66666667]))
 
 if __name__ == "__main__":
     unittest.main()
