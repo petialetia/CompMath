19ffedf17cb4dfc7d043018c227a2526a2fd42b2
petialetia <petialetia@yandex.ru>
2023-03-09 03:19:27 +0300

    better

--- 1/Tests.py
+++ 1/Tests.py
@@ -13,7 +13,7 @@ class CholeskyDecompositionTestCase(unittest.TestCase):
        result = solveSLAECholesky(coefficients_matrix, constant_terms)
        self.assertTrue((result == [5, 6]).all())
 
-    def testSimpltMatrix(self):
+    def testSimpleMatrix(self):
        coefficients_matrix = numpy.array([[2, 1], [1, 2]])
        constant_terms = [3, 4]
 
@@ -22,6 +22,17 @@ class CholeskyDecompositionTestCase(unittest.TestCase):
        
        result = solveSLAECholesky(coefficients_matrix, constant_terms)
        self.assertTrue(numpy.allclose(result, [2 / 3, 5 / 3]))
+
+    def testComplicatedMatrix(self):
+       coefficients_matrix = numpy.array([[1000, 1, 1], [1, 1000, 1], [1, 1, 1000]])
+       constant_terms = [5013, 6012, 7011]
+
+       self.assertTrue((factorizeMatrixCholesky(coefficients_matrix) == \
+                numpy.linalg.cholesky(coefficients_matrix)).all())
+       
+       result = solveSLAECholesky(coefficients_matrix, constant_terms)
+       self.assertTrue(numpy.allclose(result, [5, 6, 7]))
+
 
 if __name__ == "__main__":
     unittest.main()
