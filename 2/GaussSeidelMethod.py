7fd653ced50086b012c05d1bd9094b1c626de69f
petialetia <petialetia@yandex.ru>
2023-03-27 02:50:52 +0300

    better

--- 2/GaussSeidelMethod.py
+++ 2/GaussSeidelMethod.py
@@ -2,12 +2,13 @@ import numpy
 import itertools
 
 def main():
-    print(solveSLAEGaussSeidel(numpy.array([[1000, 1, 1], [1, 1000, 1], [1, 1, 1000]]), [5013, 6012, 7011]))
+    print(solveSLAEGaussSeidel(numpy.array([[1000, 1, 1], [1, 1000, 1], [1, 1, 1000]]), \
+            [5013, 6012, 7011]))
 
     return 0
 
-def solveSLAEGaussSeidel(coefficients_matrix, constant_terms, start_assumption = None, max_iteration = None, \
-        target_residual = 1e-08):
+def solveSLAEGaussSeidel(coefficients_matrix, constant_terms, start_assumption = None, \
+        max_iteration = None, target_residual = 1e-08):
     dimensionality = len(constant_terms)
 
     if (start_assumption is None):
@@ -21,12 +22,13 @@ def solveSLAEGaussSeidel(coefficients_matrix, constant_terms, start_assumption =
         for i in range(len(constant_terms)):
             next_step_answer[i] = (constant_terms[i] - \
                     sum(coefficients_matrix[i][j] * next_step_answer[j] for j in range(i)) - \
-                    sum(coefficients_matrix[i][j] * current_answer[j] for j in range(i + 1, dimensionality))) / \
-                    coefficients_matrix[i][i]
+                    sum(coefficients_matrix[i][j] * current_answer[j] for j in range( \
+                    i + 1, dimensionality))) / coefficients_matrix[i][i]
             
         current_answer = next_step_answer
 
-        if (numpy.allclose(coefficients_matrix @ current_answer, constant_terms, atol = target_residual)):
+        if (numpy.allclose(coefficients_matrix @ current_answer, constant_terms, \
+                atol = target_residual)):
             break
 
     return current_answer
