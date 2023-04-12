c6d985c195568eadd140a7e5c90b57f434e64ff2
liubar.pa <liubar.pa@phystech.edu>
2023-04-12 17:22:54 +0300

    better naming

--- 2/GaussSeidelMethod.py
+++ 2/GaussSeidelMethod.py
@@ -21,15 +21,15 @@ def solveSLAEGaussSeidel(coefficients_matrix, constant_terms, start_assumption =
                 atol = target_residual)):
             break
         
-        next_step_answer = numpy.zeros(dimensionality)
+        new_answer = numpy.zeros(dimensionality)
 
         for i in range(len(constant_terms)):
-            next_step_answer[i] = (constant_terms[i] - \
-                    sum(coefficients_matrix[i][j] * next_step_answer[j] for j in range(i)) - \
+            new_answer[i] = (constant_terms[i] - \
+                    sum(coefficients_matrix[i][j] * new_answer[j] for j in range(i)) - \
                     sum(coefficients_matrix[i][j] * current_answer[j] for j in range( \
                     i + 1, dimensionality))) / coefficients_matrix[i][i]
             
-        current_answer = next_step_answer
+        current_answer = new_answer
 
     return current_answer
 
