d2fc7e4c12961665b92b73ac15e75ad16f2b6092
liubar.pa <liubar.pa@phystech.edu>
2023-04-23 03:04:30 +0300

    better

--- 3/Graphics.py
+++ 3/Graphics.py
@@ -10,10 +10,18 @@ def main():
 
     left_border = -1
     right_border = 1
+    n_interpolation_nodes = 15
+    n_graphic_points = 200
 
     drawInterpolation(runge_function, \
-            getPointsUniform(runge_function, left_border, right_border, 20), \
-            getArgumentsUniform(left_border, right_border, 200), "Uniform.pdf")
+            getPoints(runge_function, left_border, right_border, \
+            n_interpolation_nodes, getArgumentsUniform), \
+            getArgumentsUniform(left_border, right_border, n_graphic_points), "Uniform.pdf")
+
+    drawInterpolation(runge_function, \
+            getPoints(runge_function, left_border, right_border, \
+            n_interpolation_nodes, getArgumentsChebyshev), \
+            getArgumentsChebyshev(left_border, right_border, n_graphic_points), "Chebyshev.pdf")
 
     return 0
 
@@ -30,9 +38,16 @@ def getArgumentsUniform(left_border, right_border, num):
 
     return arguments
 
-def getPointsUniform(function, left_border, right_border, num):
+def getArgumentsChebyshev(left_border, right_border, num):
+    return [(left_border + right_border) / 2 + \
+            (right_border - left_border) * math.cos((2 * i + 1) * math.pi / (2 * num)) / 2 \
+            for i in range(num)]
+
+def getPoints(function, left_border, right_border, num, get_arguments):
     return [Point(argument, function(argument)) for argument in 
-            getArgumentsUniform(left_border, right_border, num)]
+            get_arguments(left_border, right_border, num)]
+
+
 
 def drawInterpolation(function, nodes_of_interpolation, arguments, file_name):
     values = interpolateNewton(nodes_of_interpolation, arguments)
